ce2de1f8905b2154fdde8d6043b0ee6f1a39b9d7
pdh21 <p.d.hurley@sussex.ac.uk>
2015-03-25 14:47:43 +0000

    posterior comp codes

--- posterior_comparisons.py
+++ posterior_comparisons.py
@@ -56,8 +56,18 @@ posterior=obj['posterior']
 samples,chains,params=posterior.stan_fit.shape
 flattened_post=posterior.stan_fit.reshape(samples*chains,params)
 
+
+with open(output_folder+'Tiling_info.pkl', "rb") as f:
+        obj = pickle.load(f)
+
+tiles=obj['tiles']
+tiling_list=obj['tiling_list']
+
+
 import triangle
 sources=[773,39024]
+
+print tiling_list[sources,0],tiling_list[sources,1]
 truths=fcat_sim['S250'][idx_xidp]
 print truths[sources]
 figure = triangle.corner(flattened_post[:,sources],truths=truths[sources])
