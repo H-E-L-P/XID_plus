aa4dc3251c49946f20baea107e52d73add6fa9e1
pdh21 <p.d.hurley@sussex.ac.uk>
2015-03-25 11:43:49 +0000

    apollo updates

--- combine_tiles_posterior.py
+++ combine_tiles_posterior.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 #----output folder-----------------
-output_folder='/research/astro/fir/HELP/XID_plus_output/100micron/uniform_prior/'
+output_folder='/research/astro/fir/HELP/XID_plus_output/100micron/log_uniform_prior/'
 
 with open(output_folder+'Tiling_info.pkl', "rb") as f:
         obj = pickle.load(f)
@@ -28,7 +28,7 @@ for i in np.arange(0,len(tiles)):
     #find which sources from master list are we interested in
     ind= (np.around(tiling_list[:,2],3) == np.around(tile[0,0],3)) & (np.around(tiling_list[:,3],3) == np.around(tile[1,0],3))
     if ind.sum() >0:
-	    infile=output_folder+'lacy_uniform_fluxprior_'+str(tile[0,0]).replace('.','_')+'p'+str(tile[1,0]).replace('.','_')+'.pkl'
+	    infile=output_folder+'lacy_uniform_log10fluxprior_'+str(tile[0,0]).replace('.','_')+'p'+str(tile[1,0]).replace('.','_')+'.pkl'
 	    with open(infile, "rb") as f:
 		dictname = pickle.load(f)
 	    prior250=dictname['psw']
@@ -38,7 +38,7 @@ for i in np.arange(0,len(tiles)):
 	    print prior250.wcs._naxis1,prior250.wcs._naxis2
 	    print '---------------'
 	    posterior=dictname['posterior']
-	    #posterior.stan_fit=np.power(10.0,posterior.stan_fit)
+	    posterior.stan_fit=np.power(10.0,posterior.stan_fit)
 	    
 	    #hdulist=xid_mod.create_XIDp_SPIREcat(posterior,prior250,prior350,prior500)
 	    #table=hdulist[1].data
