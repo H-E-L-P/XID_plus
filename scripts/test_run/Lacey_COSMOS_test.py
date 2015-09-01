649ff42d419278dd08e417d332adf66f4b38cd62
pdh21 <p.d.hurley@sussex.ac.uk>
2015-09-01 17:06:54 +0100

    test_files

---
+++ scripts/test_run/Lacey_COSMOS_test.py
@@ -0,0 +1,150 @@
+import numpy as np
+import astropy
+from astropy.io import fits
+from astropy import wcs
+import pickle
+import dill
+import XIDp_mod_beta as xid_mod
+import os
+import sys
+
+#Folder containing maps
+imfolder='../../test_files/'
+#field
+field='COSMOS'
+#SMAP version
+SMAPv='4.2'
+
+
+# In[7]:
+
+pswfits=imfolder+'cosmos_itermap_lacey_07012015_simulated_observation_w_noise_PSW_hipe.fits.gz'#SPIRE 250 map
+pmwfits=imfolder+'cosmos_itermap_lacey_07012015_simulated_observation_w_noise_PMW_hipe.fits.gz'#SPIRE 350 map
+plwfits=imfolder+'cosmos_itermap_lacey_07012015_simulated_observation_w_noise_PLW_hipe.fits.gz'#SPIRE 500 map
+
+
+#----output folder-----------------
+output_folder='./'
+
+#Folder containing prior input catalogue
+folder='../../test_files/'
+#prior catalogue
+prior_cat='lacey_07012015_MillGas.ALLVOLS_cat_PSW_COSMOS_test.fits'
+
+hdulist = fits.open(folder+prior_cat)
+fcat=hdulist[1].data
+hdulist.close()
+inra=fcat['RA']
+indec=fcat['DEC']
+f_src=fcat['S100']#100 micron flux
+df_src=f_src
+nrealcat=fcat.size
+bkg250=0
+bkg350=0
+bkg500=0
+
+# Open images and noise maps and use WCS module in astropy to get header information
+
+# In[9]:
+
+#-----250-------------
+hdulist = fits.open(pswfits)
+im250phdu=hdulist[0].header
+im250hdu=hdulist[1].header
+
+im250=hdulist[1].data*1.0E3
+nim250=hdulist[2].data*1.0E3
+w_250 = wcs.WCS(hdulist[1].header)
+pixsize250=3600.0*w_250.wcs.cd[1,1] #pixel size (in arcseconds)
+hdulist.close()
+#-----350-------------
+hdulist = fits.open(pmwfits)
+im350phdu=hdulist[0].header
+im350hdu=hdulist[1].header
+
+im350=hdulist[1].data*1.0E3
+nim350=hdulist[2].data*1.0E3
+w_350 = wcs.WCS(hdulist[1].header)
+pixsize350=3600.0*w_350.wcs.cd[1,1] #pixel size (in arcseconds)
+hdulist.close()
+#-----500-------------
+hdulist = fits.open(plwfits)
+im500phdu=hdulist[0].header
+im500hdu=hdulist[1].header
+im500=hdulist[1].data*1.0E3
+nim500=hdulist[2].data*1.0E3
+w_500 = wcs.WCS(hdulist[1].header)
+pixsize500=3600.0*w_500.wcs.cd[1,1] #pixel size (in arcseconds)
+hdulist.close()
+
+
+# Since only testing, select sources within a given range of the mean ra and dec position of the prior list, and that they have a flux of greater than 50 microjanskys at 100 microns
+
+##define range
+ra_mean=np.mean(inra)
+dec_mean=np.mean(indec)
+p_range=0.05
+ 
+
+sgood=(inra > ra_mean-p_range) & (inra < ra_mean+p_range) & (indec > dec_mean-p_range) & (indec < dec_mean+p_range) & (f_src >0.050)
+
+inra=inra[sgood]
+indec=indec[sgood]
+n_src=sgood.sum()
+print 'fitting '+str(n_src)+' sources'
+
+
+
+# Point response information, at the moment its 2D Gaussian,
+
+#pixsize array (size of pixels in arcseconds)
+pixsize=np.array([pixsize250,pixsize350,pixsize500])
+#point response function for the three bands
+prfsize=np.array([18.15,25.15,36.3])
+#use Gaussian2DKernel to create prf (requires stddev rather than fwhm hence pfwhm/2.355)
+from astropy.convolution import Gaussian2DKernel
+
+
+#Set prior classes
+#---prior250--------
+prior250=xid_mod.prior(im250,nim250,im250phdu,im250hdu)#Initialise with map, uncertianty map, wcs info and primary header
+prior250.prior_cat(inra,indec,prior_cat)#Set input catalogue
+prior250.prior_bkg(bkg250,5)#Set prior on background
+#---prior350--------
+prior350=xid_mod.prior(im350,nim350,im350phdu,im350hdu)
+prior350.prior_cat(inra,indec,prior_cat)
+prior350.prior_bkg(bkg350,5)
+#---prior500--------
+prior500=xid_mod.prior(im500,nim500,im500phdu,im500hdu)
+prior500.prior_cat(inra,indec,prior_cat)
+prior500.prior_bkg(bkg500,5)
+
+
+##---------fit using Gaussian beam-----------------------
+prf250=Gaussian2DKernel(prfsize[0]/2.355,x_size=101,y_size=101)
+prf250.normalize(mode='peak')
+prf350=Gaussian2DKernel(prfsize[1]/2.355,x_size=101,y_size=101)
+prf350.normalize(mode='peak')
+prf500=Gaussian2DKernel(prfsize[2]/2.355,x_size=101,y_size=101)
+prf500.normalize(mode='peak')
+
+pind250=np.arange(0,101,1)*1.0/pixsize[0] #get 250 scale in terms of pixel scale of map
+pind350=np.arange(0,101,1)*1.0/pixsize[1] #get 350 scale in terms of pixel scale of map
+pind500=np.arange(0,101,1)*1.0/pixsize[2] #get 500 scale in terms of pixel scale of map
+
+prior250.set_prf(prf250.array,pind250,pind250)
+prior350.set_prf(prf350.array,pind350,pind350)
+prior500.set_prf(prf500.array,pind500,pind500)
+
+prior250.get_pointing_matrix()
+prior350.get_pointing_matrix()
+prior500.get_pointing_matrix()
+
+fit=xid_mod.lstdrv_SPIRE_stan(prior250,prior350,prior500,iter=1500)
+posterior=xid_mod.posterior_stan(fit,prior250.nsrc)
+#----------------------------------------------------------
+
+outfile=output_folder+'Lacy_test_file.pkl'
+with open(outfile, 'wb') as f:
+    pickle.dump({'psw':prior250,'pmw':prior350,'plw':prior500,'posterior':posterior},f)
+
