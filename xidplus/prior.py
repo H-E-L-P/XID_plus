e19484a82bc97dc6699abf44c90c06fadc52668d
pdh21 <p.d.hurley@sussex.ac.uk>
2015-09-09 13:06:02 +0100

    move background out of pointing matrix to speed up code

--- xidplus/prior.py
+++ xidplus/prior.py
@@ -160,26 +160,17 @@ class prior(object):
                 amat_row=np.append(amat_row,np.arange(0,self.snpix,dtype=int)[good])#what pixels the source contributes to
                 amat_col=np.append(amat_col,np.full(ngood,s))#what source we are on
         
-        #Add background contribution to pointing matrix: 
-        #only contributes to pixels within tile
-        if bkg == True:
-            snpix_bkg=self.snpix
-            self.amat_data=np.append(amat_data,np.full(snpix_bkg,1))
-            self.amat_row=np.append(amat_row,np.arange(0,self.snpix,dtype=int))
-            self.amat_col=np.append(amat_col,np.full(snpix_bkg,s+1))
-        else:
-            ind=np.unique(amat_row).astype(int) # only add backround contribution to those pixels that prior sources contribute to
-            snpix_bkg=ind.size
-            self.amat_data=np.append(amat_data,np.full(snpix_bkg,1))
-            self.amat_row=np.append(amat_row,ind)
-            self.amat_col=np.append(amat_col,np.full(snpix_bkg,s+1))
+
+            self.amat_data=amat_data
+            self.amat_row=amat_row
+            self.amat_col=amat_col
 
 
     def get_pointing_matrix_coo(self):
         """Get scipy coo version of pointing matrix. Useful for sparse matrix multiplication"""
         from scipy.sparse import coo_matrix
         self.A=coo_matrix((self.amat_data, (self.amat_row, self.amat_col)), shape=(self.snpix, self.nsrc))
-    
+
     def cut_map_to_prior(self):
         """If only interested in fitting around regions of prior objects, run this function to cut down amount of data being fitted to."""
         ind=np.unique(self.amat_row).astype(int)
